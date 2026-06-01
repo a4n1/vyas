fba6e0daed308a4b0c521b3bdd7d4f55367a445e
Agni <a@a4n1.com>
2026-06-01 20:59:12 +0100

    add face culling
    Signed-off-by: Agni <a@a4n1.com>

--- crates/vyas/src/mesh.rs
+++ crates/vyas/src/mesh.rs
@@ -16,13 +16,15 @@ impl Mesh {
     ) -> Mesh {
         let mut vertices: Vec<Vertex> = Vec::new();
         let mut indices: Vec<u32> = Vec::new();
+        let mut indices_index = 0;
 
-        for (index, (local_position, voxel)) in voxels.iter().enumerate() {
+        for (local_position, voxel) in voxels {
             let (v, i) = build_voxel(
                 chunk_position,
                 local_position,
                 voxel,
-                (index as u32) * 8,
+                &mut indices_index,
+                voxels,
                 render_config,
             );
 
@@ -38,7 +40,8 @@ fn build_voxel(
     chunk_position: &GridPosition,
     local_position: &GridPosition,
     voxel: &Voxel,
-    indices_index: u32,
+    indices_index: &mut u32,
+    voxels: &HashMap<GridPosition, Voxel>,
     render_config: &RenderConfig,
 ) -> (Vec<Vertex>, Vec<u32>) {
     let color = [voxel.color.r(), voxel.color.g(), voxel.color.b()];
@@ -52,87 +55,137 @@ fn build_voxel(
     let z = (chunk_position.z * render_config.chunk_size as i32 + local_position.z) as f32
         * render_config.voxel_size;
 
-    let vertices = vec![
-        // Front Face
-        Vertex {
-            position: [x - half_size, y - half_size, z + half_size],
-            color,
-        },
-        Vertex {
-            position: [x + half_size, y - half_size, z + half_size],
-            color,
-        },
-        Vertex {
-            position: [x + half_size, y + half_size, z + half_size],
-            color,
-        },
-        Vertex {
-            position: [x - half_size, y + half_size, z + half_size],
-            color,
-        },
-        // Rear Face
-        Vertex {
-            position: [x - half_size, y - half_size, z - half_size],
-            color,
-        },
-        Vertex {
-            position: [x + half_size, y - half_size, z - half_size],
-            color,
-        },
-        Vertex {
-            position: [x + half_size, y + half_size, z - half_size],
-            color,
-        },
-        Vertex {
-            position: [x - half_size, y + half_size, z - half_size],
-            color,
-        },
-    ];
+    let mut vertices = Vec::with_capacity(24);
+    let mut indices = Vec::with_capacity(36);
+
+    for direction in [
+        Direction::Front,
+        Direction::Back,
+        Direction::Left,
+        Direction::Right,
+        Direction::Top,
+        Direction::Bottom,
+    ] {
+        let is_adjacent_face = match direction {
+            Direction::Left => voxels.contains_key(&GridPosition {
+                x: local_position.x - 1,
+                y: local_position.y,
+                z: local_position.z,
+            }),
+            Direction::Right => voxels.contains_key(&GridPosition {
+                x: local_position.x + 1,
+                y: local_position.y,
+                z: local_position.z,
+            }),
+            Direction::Top => voxels.contains_key(&GridPosition {
+                x: local_position.x,
+                y: local_position.y + 1,
+                z: local_position.z,
+            }),
+            Direction::Bottom => voxels.contains_key(&GridPosition {
+                x: local_position.x,
+                y: local_position.y - 1,
+                z: local_position.z,
+            }),
+            Direction::Front => voxels.contains_key(&GridPosition {
+                x: local_position.x,
+                y: local_position.y,
+                z: local_position.z + 1,
+            }),
+            Direction::Back => voxels.contains_key(&GridPosition {
+                x: local_position.x,
+                y: local_position.y,
+                z: local_position.z - 1,
+            }),
+        };
+
+        if is_adjacent_face {
+            continue;
+        }
+
+        let (quad_vertices, quad_indices) =
+            generate_quad(direction, x, y, z, half_size, *indices_index, color);
+
+        *indices_index += quad_vertices.len() as u32;
+
+        vertices.extend(quad_vertices);
+        indices.extend(quad_indices);
+    }
+
+    (vertices, indices)
+}
+
+#[derive(Clone, Copy)]
+enum Direction {
+    Left,
+    Right,
+    Top,
+    Bottom,
+    Front,
+    Back,
+}
+
+fn generate_quad(
+    direction: Direction,
+    x: f32,
+    y: f32,
+    z: f32,
+    half_size: f32,
+    indices_index: u32,
+    color: [f32; 3],
+) -> (Vec<Vertex>, Vec<u32>) {
+    let corners = match direction {
+        Direction::Front => [
+            [x - half_size, y - half_size, z + half_size],
+            [x + half_size, y - half_size, z + half_size],
+            [x + half_size, y + half_size, z + half_size],
+            [x - half_size, y + half_size, z + half_size],
+        ],
+        Direction::Back => [
+            [x + half_size, y - half_size, z - half_size],
+            [x - half_size, y - half_size, z - half_size],
+            [x - half_size, y + half_size, z - half_size],
+            [x + half_size, y + half_size, z - half_size],
+        ],
+        Direction::Left => [
+            [x - half_size, y - half_size, z - half_size],
+            [x - half_size, y - half_size, z + half_size],
+            [x - half_size, y + half_size, z + half_size],
+            [x - half_size, y + half_size, z - half_size],
+        ],
+        Direction::Right => [
+            [x + half_size, y - half_size, z + half_size],
+            [x + half_size, y - half_size, z - half_size],
+            [x + half_size, y + half_size, z - half_size],
+            [x + half_size, y + half_size, z + half_size],
+        ],
+        Direction::Top => [
+            [x - half_size, y + half_size, z + half_size],
+            [x + half_size, y + half_size, z + half_size],
+            [x + half_size, y + half_size, z - half_size],
+            [x - half_size, y + half_size, z - half_size],
+        ],
+        Direction::Bottom => [
+            [x - half_size, y - half_size, z - half_size],
+            [x + half_size, y - half_size, z - half_size],
+            [x + half_size, y - half_size, z + half_size],
+            [x - half_size, y - half_size, z + half_size],
+        ],
+    };
+
+    let vertices = corners
+        .into_iter()
+        .map(|position| Vertex { position, color })
+        .collect();
 
     #[allow(clippy::identity_op)]
     let indices = vec![
-        // Front
         indices_index + 0,
         indices_index + 1,
         indices_index + 2,
         indices_index + 2,
         indices_index + 3,
         indices_index + 0,
-        // Back
-        indices_index + 5,
-        indices_index + 4,
-        indices_index + 7,
-        indices_index + 7,
-        indices_index + 6,
-        indices_index + 5,
-        // Left
-        indices_index + 4,
-        indices_index + 0,
-        indices_index + 3,
-        indices_index + 3,
-        indices_index + 7,
-        indices_index + 4,
-        // Right
-        indices_index + 1,
-        indices_index + 5,
-        indices_index + 6,
-        indices_index + 6,
-        indices_index + 2,
-        indices_index + 1,
-        // Top
-        indices_index + 3,
-        indices_index + 2,
-        indices_index + 6,
-        indices_index + 6,
-        indices_index + 7,
-        indices_index + 3,
-        // Bottom
-        indices_index + 4,
-        indices_index + 5,
-        indices_index + 1,
-        indices_index + 1,
-        indices_index + 0,
-        indices_index + 4,
     ];
 
     (vertices, indices)
